bb8d24f6a587ddcf845b49ed823aef0e9da67ce1
Akanksha Singh <79742074+akanksha57@users.noreply.github.com>
2021-10-18 21:19:42 +0530

    Create pyramid.py

---
+++ pyramid.py
@@ -0,0 +1,28 @@
+
+def triangle(n):
+	
+	# number of spaces
+	k = n - 1
+
+
+	for i in range(0, n):
+	
+	
+		for j in range(0, k):
+			print(end=" ")
+	
+		
+		k = k - 1
+	
+	
+		for j in range(0, i+1):
+		
+			# printing stars
+			print("* ", end="")
+	
+		
+		print("\r")
+
+# Driver Code
+n = 5
+triangle(n)
